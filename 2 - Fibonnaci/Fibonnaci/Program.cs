8cd06ce7764c74eed43b9de8b00a197b9194d530
CleitonDaniluski <cleiton.daniluski@hotmail.com>
2024-03-06 12:00:50 -0300

    commit final

---
+++ 2 - Fibonnaci/Fibonnaci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int numero = 34; // Você pode alterar este valor para testar outros números
+
+        bool pertence = PertenceFibonacci(numero);
+
+        if (pertence)
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool PertenceFibonacci(int numero)
+    {
+      
+        int a = 0;
+        int b = 1;
+
+        while (b < numero)
+        {
+            int proximo = a + b;
+
+            a = b;
+            b = proximo;
+        }
+
+        return b == numero;
+    }
+}
---
+++ 5 - InverterString/InverterString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string entrada = "Teste para Estágio Ribeirão Preto!";
+
+        string invertida = InverterString(entrada);
+
+        Console.WriteLine("String original: " + entrada);
+        Console.WriteLine("String invertida: " + invertida);
+    }
+
+    static string InverterString(string str)
+    {
+        char[] caracteres = str.ToCharArray();
+
+        string invertida = "";
+
+        for (int i = caracteres.Length - 1; i >= 0; i--)
+        {
+            invertida += caracteres[i];
+        }
+
+        return invertida;
+    }
+}
